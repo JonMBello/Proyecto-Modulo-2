c4c633268352dab614057ccb33969098b065cb76
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-02-04 15:57:14 -0600

    Rama de desarrollo

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //Función que agrega nuevas tasks a la lista.
 function newTask() {
-
+    //Code goes here
 }
 //Función que marca una task como completada.
 function taskComplete(id) {
-
+    //Code goes here
 }
 //Función que elimina una task que no necesite.
 function deleteTask(id) {
-
+    //Code goes here
 }
