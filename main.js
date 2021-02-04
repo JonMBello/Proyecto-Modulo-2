a83641135d9e92c7ec6e48705260489f812ac00e
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-02-04 17:40:27 -0600

    "Correción del formulario en el index"

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//Función que crea la maqueta
+function initTodoApp() {
+    //Code goes here
+}
+initTodoApp();
+
 //Función que agrega nuevas tasks a la lista.
 function newTask() {
     //Code goes here
