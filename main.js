9a52874f2a4537dfda0431fd3ac2979e30a2b0d5
Jonathan Martinez <jonathanmtzbello@gmail.com>
2021-02-04 15:53:02 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//Función que agrega nuevas tasks a la lista.
+function newTask() {
+
+}
+//Función que marca una task como completada.
+function taskComplete(id) {
+
+}
+//Función que elimina una task que no necesite.
+function deleteTask(id) {
+
+}
