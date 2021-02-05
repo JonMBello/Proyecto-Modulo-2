8588f158697dae299655332ad87821eb8d6577fe
ulises801 <ulises-801@outlook.es>
2021-02-05 16:49:55 -0600

    funcion taskComplete()

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+//Función que crea la maqueta
+function initTodoApp() {
+    //Code goes here
+}
+initTodoApp();
+
 //Función que agrega nuevas tasks a la lista.
 function newTask() {
-
+    //Code goes here
 }
 //Función que marca una task como completada.
 function taskComplete(id) {
+    var label = document.getElementById(`labelTask${id}`);
+    var checkBox = document.getElementById(`checkTask${id}`);
 
+    if (checkBox.checked == true){
+        label.style.textDecoration = "line-through";
+        label.style.color = "green";
+      } else {
+        label.style.textDecoration = "";
+        label.style.color = "black";
+      }
 }
 //Función que elimina una task que no necesite.
 function deleteTask(id) {
-
+    //Code goes here
 }
